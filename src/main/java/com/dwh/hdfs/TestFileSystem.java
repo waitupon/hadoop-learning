cbe0b6066c33317aa4cfb9fe0939d2eda4066380
dingwenhao <dingwenhao@baiwang.com>
2018-05-16 14:43:13 +0800

    fist

---
+++ src/main/java/com/dwh/hdfs/TestFileSystem.java
@@ -0,0 +1,16 @@
+package com.dwh.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2018/5/16 0016.
+ */
+public class TestFileSystem {
+
+    @Test
+    public void testWrite(){
+        Configuration conf = new Configuration();
+    }
+
+}
