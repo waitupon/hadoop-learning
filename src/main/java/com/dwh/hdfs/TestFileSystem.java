13ad5db78a6d4330f10f1149681df19f93c70171
config-user <376943653@qq.com>
2018-05-16 17:10:22 +0800

    test

--- src/main/java/com/dwh/hdfs/TestFileSystem.java
+++ src/main/java/com/dwh/hdfs/TestFileSystem.java
@@ -1,28 +1,87 @@
 package com.dwh.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.io.IOUtils;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 /**
  * Created by Administrator on 2018/5/16 0016.
  */
 public class TestFileSystem {
 
+    Configuration conf = null;
+
+    FileSystem fs = null;
+
+    @Before
+    public void initConf(){
+        Configuration conf = new Configuration();
+        try {
+            FileSystem fs = FileSystem.get(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void testWrite() throws Exception {
-        Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(conf);
         FSDataOutputStream dos = fs.create(new Path("hdfs://master:9000/usr/hello.txt"));
-
         dos.write("hello".getBytes());
         dos.close();
         System.out.println("over");
 
+    }
+
+
+    @Test
+    public void testRead() throws Exception {
+        FSDataInputStream dis = fs.open(new Path("hdfs://master:9000/usr/hello.txt"));
+        FileOutputStream fos = new FileOutputStream("d:/hello.txt");
+        IOUtils.copyBytes(dis,fos,1024);
+        IOUtils.closeStream(dis);
+        IOUtils.closeStream(fos);
+    }
+
+
+    @Test
+    public void testSeek() throws Exception {
+        FSDataInputStream dis = fs.open(new Path("hdfs://master:9000/usr/hello.txt"));
+        FileOutputStream fos = new FileOutputStream("d:/hello.txt");
+        dis.seek(2);
+        IOUtils.copyBytes(dis,fos,1024);
+        IOUtils.closeStream(dis);
+        IOUtils.closeStream(fos);
+    }
+
+    @Test
+    public void mkdir() throws Exception {
+        Path path = new Path("/usr/myfolder");
+        boolean b = fs.mkdirs(path, FsPermission.getDefault());
+        System.out.println(b);
+    }
+
+
+    @Test
+    public void testStatus() throws Exception {
+        FileStatus fileStatus = fs.getFileStatus(new Path("/"));
+        Class clazz = fileStatus.getClass();
+        Method[] methods = clazz.getMethods();
+        for(Method m : methods){
+            Class[] parameterTypes = m.getParameterTypes();
+            if(m.getName().startsWith("get")  && (parameterTypes==null || parameterTypes.length ==0)){
+                if(!m.getName().equals("getSymlink")){
+                    Object ret = m.invoke(fileStatus, null);
+                    System.out.println(m.getName() + "()=" + ret);
+                }
+            }
+        }
     }
 
 
