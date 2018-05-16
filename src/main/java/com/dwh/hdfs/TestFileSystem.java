23039e2c5d63d067b04eaa0b407ac472471ea68f
dingwenhao <dingwenhao@baiwang.com>
2018-05-16 15:19:36 +0800

    testFs

--- src/main/java/com/dwh/hdfs/TestFileSystem.java
+++ src/main/java/com/dwh/hdfs/TestFileSystem.java
@@ -1,7 +1,12 @@
 package com.dwh.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2018/5/16 0016.
@@ -9,8 +14,16 @@ import org.junit.Test;
 public class TestFileSystem {
 
     @Test
-    public void testWrite(){
+    public void testWrite() throws Exception {
         Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        FSDataOutputStream dos = fs.create(new Path("hdfs://master:9000/usr/hello.txt"));
+
+        dos.write("hello".getBytes());
+        dos.close();
+        System.out.println("over");
+
     }
+
 
 }
