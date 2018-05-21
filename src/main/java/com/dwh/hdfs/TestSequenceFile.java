4e941609264316d5bcae140a4c5080364ab6bc66
config-user <376943653@qq.com>
2018-05-21 23:38:29 +0800

    测试串行化

--- src/main/java/com/dwh/hdfs/TestSequenceFile.java
+++ src/main/java/com/dwh/hdfs/TestSequenceFile.java
@@ -6,6 +6,10 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.DeflateCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.io.compress.SnappyCodec;
+import org.apache.hadoop.util.ReflectionUtils;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -40,7 +44,7 @@ public class TestSequenceFile {
     public void readSeq() throws Exception {
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(conf);
-        Path path = new Path("/usr/my.seq");
+        Path path = new Path("/usr/my2.seq");
 
         SequenceFile.Reader reader = new SequenceFile.Reader(fs, path, conf);
 
@@ -55,4 +59,34 @@ public class TestSequenceFile {
         System.out.println("over");
 
     }
-}
+
+
+
+    @Test
+    public void compressSeq() throws Exception {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        Path path = new Path("/usr/my3.seq");
+
+        //SnappyCodec codec = ReflectionUtils.newInstance(SnappyCodec.class,conf);
+        DeflateCodec codec = ReflectionUtils.newInstance(DeflateCodec.class,conf);
+
+
+        SequenceFile.Writer writer = SequenceFile.createWriter(conf,fs.create(path), IntWritable.class, Text.class,SequenceFile.CompressionType.BLOCK,codec);
+
+        IntWritable key = new IntWritable();
+        Text value = new Text();
+
+        for(int i=0;i<5;i++){
+            key.set(i);
+            value.set("itaits" + i);
+            writer.append(key,value);
+        }
+        writer.close();
+        System.out.println("over");
+
+    }
+
+
+
+    }
---
+++ src/main/java/serializable/Dog.java
@@ -0,0 +1,19 @@
+package serializable;
+
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
