674b770df3ba3c5a21e7d01cd26e47d24ae4c56b
dingwenhao <dingwenhao@baiwang.com>
2018-08-08 20:45:39 +0800

    kafka add

--- src/main/java/kafka/App.java
+++ src/main/java/kafka/App.java
@@ -1,5 +1,13 @@
 package kafka;
 
+
+
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.MessageAndMetadata;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -10,8 +18,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by Administrator on 2018/8/6 0006.
@@ -19,7 +26,7 @@ import java.util.Properties;
 public class App {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.3.172:9092");
+        props.put("bootstrap.servers", "master:9092");
         props.put("acks", "all");
         props.put("retries", 0);
         props.put("batch.size", 16384);
@@ -30,7 +37,7 @@ public class App {
         //生产者发送消息
         String topic = "mytopic";
         Producer<String, String> procuder = new KafkaProducer<String,String>(props);
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 2; i++) {
             String value = "val_" + i;
             ProducerRecord<String, String> msg = new ProducerRecord<String, String>(topic, value);
 
@@ -46,21 +53,32 @@ public class App {
 
 
     @Test
-    public void mostOnce(){
+    public void consumer(){
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.3.172:9092");
-//        props.put("group.id", "test-1");
-        props.put("enable.auto.commit", "true");
+        props.put("zookeeper.connect", "master:2181");
+        props.put("group.id", "test-1");
         props.put("auto.commit.interval.ms", "1000");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-        consumer.subscribe(Arrays.asList("mytopic", "bar"));
 
-            ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println(record.key() +"," + record.value());
+
+
+        ConsumerConfig conf = new ConsumerConfig(props);
+
+        ConsumerConnector conn = Consumer.createJavaConsumerConnector(conf);
+
+        Map<String,Integer> topicCount = new HashMap<String,Integer>();
+        topicCount.put("mytopic",1);
+
+        Map<String, List<KafkaStream<byte[], byte[]>>> map = conn.createMessageStreams(topicCount);
+        List<KafkaStream<byte[], byte[]>> list = map.get("mytopic");
+        for(KafkaStream<byte[], byte[]> data :list){
+            ConsumerIterator<byte[], byte[]> iterator = data.iterator();
+            while(iterator.hasNext()){
+                MessageAndMetadata<byte[], byte[]> message = iterator.next();
+                System.out.println( new String(message.message()));
             }
+
+        }
+        conn.shutdown();
 
     }
 }
